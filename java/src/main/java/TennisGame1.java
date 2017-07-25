f65731c2fd7512c72ba5d5630da7b6045845541d
littlestar-dao <dao.duan@hotmail.com>
2017-07-25 10:21:18 +0700

    combine normal case

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -33,42 +33,37 @@ public class TennisGame1 implements TennisGame {
         } else if (isWinner()) {
             if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
-        } else if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (normal()) {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
+        }else if (normal()) {
+            if (m_score1 == m_score2) {
+                switch (m_score1) {
                     case 1:
-                        score += "Fifteen";
+                        score = "Fifteen-All";
                         break;
                     case 2:
-                        score += "Thirty";
+                        score = "Thirty-All";
                         break;
-                    case 3:
-                        score += "Forty";
-                        break;
+                }
+            }else{
+                for (int i = 1; i < 3; i++) {
+                    if (i == 1) tempScore = m_score1;
+                    else {
+                        score += "-";
+                        tempScore = m_score2;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score += "Love";
+                            break;
+                        case 1:
+                            score += "Fifteen";
+                            break;
+                        case 2:
+                            score += "Thirty";
+                            break;
+                        case 3:
+                            score += "Forty";
+                            break;
+                    }
                 }
             }
         }
