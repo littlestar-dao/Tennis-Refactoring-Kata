e3fd32f68d9ff91f2d869f8e35da9eb3c2f64356
littlestar-dao <dao.duan@hotmail.com>
2017-07-25 10:15:51 +0700

    create normal() method

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,6 +5,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private int countDuece = 0;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -24,23 +25,15 @@ public class TennisGame1 implements TennisGame {
         int minusResult = m_score1 - m_score2;
         if (m_score1 == 0 && m_score2 == 0) {
             return "Love-All";
-
-//        } else if (normal()) {
-
         } else if (isDuece()) {
             score = "Deuce";
-
         } else if (isAdvantage()) {
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-
+            if (minusResult == 1) score = "Advantage " + this.player1Name;
+            else if (minusResult == -1) score = "Advantage " + this.player2Name;
         } else if (isWinner()) {
             if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
-        }
-
-
-        else if (m_score1 == m_score2) {
+        } else if (m_score1 == m_score2) {
             switch (m_score1) {
                 case 0:
                     score = "Love-All";
@@ -56,7 +49,7 @@ public class TennisGame1 implements TennisGame {
                     break;
 
             }
-        } else {
+        } else if (normal()) {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = m_score1;
                 else {
@@ -82,27 +75,36 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
-    public boolean isDuece(){
-        if (m_score1 == m_score2 && m_score1 >=3 && m_score2 >=3) {
+    public boolean isDuece() {
+        if (m_score1 == m_score2 && m_score1 >= 3 && m_score2 >= 3) {
+            countDuece++;
             return true;
         }
         return false;
     }
 
-    public boolean isAdvantage(){
-        if (m_score1 >= 4 || m_score2 >= 4){
+    public boolean isAdvantage() {
+        if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if(minusResult == 1 || minusResult == -1) {
+            if (minusResult == 1 || minusResult == -1) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean isWinner(){
+    public boolean isWinner() {
         int minusResult = m_score1 - m_score2;
-        if (m_score1 >= 4 || m_score2 >= 4 && ((minusResult >= 2) || (minusResult <= -2))){
+        if (m_score1 >= 4 || m_score2 >= 4 && ((minusResult >= 2) || (minusResult <= -2))) {
             return true;
-        }return false;
+        }
+        return false;
+    }
+
+    public boolean normal() {
+        if (countDuece <= 0) {
+            return true;
+        }
+        return false;
     }
 }
